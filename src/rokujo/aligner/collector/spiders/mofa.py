cf3432458c2290428ebfee38c49ddf2178e51de3
Tomoyuki Sakurai <y@trombik.org>
2026-09-09 01:15:11 +0700

    collector: a spider for MoFA

---
+++ src/rokujo/aligner/collector/spiders/mofa.py
@@ -0,0 +1,31 @@
+from scrapy.http import Response
+
+from .base_spider import BaseBilingualArticleSpider
+
+
+class MofaSpider(BaseBilingualArticleSpider):
+    name = "mofa"
+    allowed_domains = ["mofa.go.jp"]
+    start_urls = ["https://www.mofa.go.jp/whats/index.html"]
+    article_xpath = "//ul[@class='link-list']/li//@href"
+    source_xpath = "//div[contains(@class, 'other-language')]//a/@href"
+    swap_target_and_source: bool = False
+
+    def parse_archive_index(self, response: Response):
+        all_index_pages = response.xpath(
+            "//tbody[contains(@class, 'archives-body')]//a/@href"
+        ).getall()
+
+        if all_index_pages:
+            for index_page in all_index_pages:
+                yield response.follow(
+                    index_page, callback=self.parse_monthly_index
+                )
+
+    def parse_monthly_index(self, response: Response):
+        article_hrefs = response.xpath(self.article_xpath).getall()
+        for article_href in article_hrefs:
+            self.logger.debug(f"Found article href: {article_href}")
+            yield response.follow(
+                article_href, callback=self.parse_target_article
+            )
